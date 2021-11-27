6f1cd9acb358f82899ca0f22976e5ef5667d6f85
Manuel Ferreras <35044975+ManuelFerreras@users.noreply.github.com>
2021-11-26 23:52:59 -0300

    Create comments.sol

---
+++ comments.sol
@@ -0,0 +1,7 @@
+// Formato natspec para comentar en solidity
+
+/// @title <Título del Contrato>
+/// @author <Autor del Contrato>
+/// @notice <Explicar lo que hace el contrato o la Función>
+/// @dev <Detalles Adicionales Sobre el Contrato o Función>
+/// @param <nombre_parametro> <Describir para qué sirve el valor de retorno de una funcion>
